8062d56e0f197e14be957e5427b31b83b8fcf8ac
Atlaxsyys <y.shafran-06@mail.ru>
2026-04-06 17:56:04 +0300

    [codegen]: add configurable lowering pipeline options for LLVM conversion passes

--- include/codegen/lowering_pipeline.hpp
+++ include/codegen/lowering_pipeline.hpp
@@ -1,9 +1,24 @@
 #pragma once
 
+#include <string>
+
 #include "mlir/IR/BuiltinOps.h"
 #include "mlir/Support/LogicalResult.h"
 
 namespace tc
 {
-    mlir::LogicalResult run_lowering_pipeline (mlir::ModuleOp module);
+    struct LoweringPipelineOptions
+    {
+        bool enable_verifier = true;
+        bool bufferize_function_boundaries = true;
+        unsigned index_bitwidth = 0;
+        bool use_bare_ptr_call_conv = false;
+        bool use_aligned_alloc = false;
+        std::string llvm_data_layout;
+    };
+
+    mlir::LogicalResult run_lowering_pipeline (
+        mlir::ModuleOp module,
+        const LoweringPipelineOptions& options = {}
+    );
 } // namespace tc
--- src/codegen/lowering_pipeline.cpp
+++ src/codegen/lowering_pipeline.cpp
@@ -8,40 +8,64 @@
 
 namespace tc
 {
-    mlir::LogicalResult run_lowering_pipeline (mlir::ModuleOp module)
+    mlir::LogicalResult run_lowering_pipeline (
+        mlir::ModuleOp module,
+        const LoweringPipelineOptions& options
+    )
     {
         mlir::PassManager pm (module.getContext());
 
-        pm.enableVerifier (true);
+        pm.enableVerifier (options.enable_verifier);
 
-        pm.addPass (mlir::createCanonicalizerPass());
-        pm.addPass (mlir::createCSEPass());
-
-        mlir::bufferization::OneShotBufferizePassOptions bufferize_options;
-        bufferize_options.bufferizeFunctionBoundaries = true;
-
-        pm.addPass (mlir::bufferization::createOneShotBufferizePass (bufferize_options));
-        pm.addPass (mlir::createCanonicalizerPass());
-        pm.addPass (mlir::createCSEPass());
-
-        pm.addPass (mlir::createConvertLinalgToLoopsPass());
-        pm.addPass (mlir::createCanonicalizerPass());
-        pm.addPass (mlir::createCSEPass());
-
-        pm.addPass (mlir::createSCFToControlFlowPass());
-        pm.addPass (mlir::createCanonicalizerPass());
-        pm.addPass (mlir::createCSEPass());
-
-        pm.addPass (mlir::createArithToLLVMConversionPass());
-        pm.addPass (mlir::createConvertIndexToLLVMPass());
-        pm.addPass (mlir::createConvertControlFlowToLLVMPass());
-        pm.addPass (mlir::createFinalizeMemRefToLLVMConversionPass());
-        pm.addPass (mlir::createConvertFuncToLLVMPass());
-
-        pm.addPass (mlir::createCanonicalizerPass());
+        pm.addPass (mlir::createCanonicalizerPass ());
         pm.addPass (mlir::createCSEPass ());
 
-        pm.addPass (mlir::createReconcileUnrealizedCastsPass());
+        mlir::bufferization::OneShotBufferizePassOptions bufferize_options;
+        bufferize_options.bufferizeFunctionBoundaries = options.bufferize_function_boundaries;
+
+        pm.addPass (mlir::bufferization::createOneShotBufferizePass (bufferize_options));
+        pm.addPass (mlir::createCanonicalizerPass ());
+        pm.addPass (mlir::createCSEPass ());
+
+        pm.addPass (mlir::createConvertLinalgToLoopsPass ());
+        pm.addPass (mlir::createCanonicalizerPass ());
+        pm.addPass (mlir::createCSEPass ());
+
+        pm.addPass (mlir::createSCFToControlFlowPass ());
+        pm.addPass (mlir::createCanonicalizerPass ());
+        pm.addPass (mlir::createCSEPass ());
+
+        mlir::ArithToLLVMConversionPassOptions arith_options;
+        arith_options.indexBitwidth = options.index_bitwidth;
+
+        mlir::ConvertIndexToLLVMPassOptions index_options;
+        index_options.indexBitwidth = options.index_bitwidth;
+
+        mlir::FinalizeMemRefToLLVMConversionPassOptions memref_options;
+        memref_options.indexBitwidth = options.index_bitwidth;
+        memref_options.useAlignedAlloc = options.use_aligned_alloc;
+
+        mlir::ConvertFuncToLLVMPassOptions func_options;
+        func_options.indexBitwidth = options.index_bitwidth;
+        func_options.useBarePtrCallConv = options.use_bare_ptr_call_conv;
+
+        pm.addPass (mlir::createArithToLLVMConversionPass (arith_options));
+        pm.addPass (mlir::createConvertIndexToLLVMPass (index_options));
+        pm.addPass (mlir::createConvertControlFlowToLLVMPass ());
+        pm.addPass (mlir::createFinalizeMemRefToLLVMConversionPass (memref_options));
+        pm.addPass (mlir::createConvertFuncToLLVMPass (func_options));
+
+        if (!options.llvm_data_layout.empty ())
+        {
+            mlir::SetLLVMModuleDataLayoutPassOptions data_layout_options;
+            data_layout_options.dataLayout = options.llvm_data_layout;
+            pm.addPass (mlir::createSetLLVMModuleDataLayoutPass (data_layout_options));
+        }
+
+        pm.addPass (mlir::createCanonicalizerPass ());
+        pm.addPass (mlir::createCSEPass ());
+
+        pm.addPass (mlir::createReconcileUnrealizedCastsPass ());
 
         return pm.run (module);
     }
