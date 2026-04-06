46f1db81e3bebd81f8bd87654a28c5f2ca613eb6
Atlaxsyys <y.shafran-06@mail.ru>
2026-04-06 17:44:16 +0300

    [codegen]: implemented initial MLIP loweriong pipeline to LLVM dialect

--- src/codegen/lowering_pipeline.cpp
+++ src/codegen/lowering_pipeline.cpp
@@ -1,8 +1,10 @@
 #include "codegen/lowering_pipeline.hpp"
 #include "mlir/Pass/PassManager.h"
 #include "mlir/Transforms/Passes.h"
-
-
+#include "mlir/Dialect/Bufferization/Transforms/Passes.h"
+#include "mlir/Dialect/Linalg/Passes.h"
+#include "mlir/Conversion/SCFToControlFlow/SCFToControlFlow.h"
+#include "mlir/Conversion/Passes.h"
 
 namespace tc
 {
@@ -13,7 +15,33 @@ namespace tc
         pm.enableVerifier (true);
 
         pm.addPass (mlir::createCanonicalizerPass());
+        pm.addPass (mlir::createCSEPass());
+
+        mlir::bufferization::OneShotBufferizePassOptions bufferize_options;
+        bufferize_options.bufferizeFunctionBoundaries = true;
+
+        pm.addPass (mlir::bufferization::createOneShotBufferizePass (bufferize_options));
+        pm.addPass (mlir::createCanonicalizerPass());
+        pm.addPass (mlir::createCSEPass());
+
+        pm.addPass (mlir::createConvertLinalgToLoopsPass());
+        pm.addPass (mlir::createCanonicalizerPass());
+        pm.addPass (mlir::createCSEPass());
+
+        pm.addPass (mlir::createSCFToControlFlowPass());
+        pm.addPass (mlir::createCanonicalizerPass());
+        pm.addPass (mlir::createCSEPass());
+
+        pm.addPass (mlir::createArithToLLVMConversionPass());
+        pm.addPass (mlir::createConvertIndexToLLVMPass());
+        pm.addPass (mlir::createConvertControlFlowToLLVMPass());
+        pm.addPass (mlir::createFinalizeMemRefToLLVMConversionPass());
+        pm.addPass (mlir::createConvertFuncToLLVMPass());
+
+        pm.addPass (mlir::createCanonicalizerPass());
         pm.addPass (mlir::createCSEPass ());
+
+        pm.addPass (mlir::createReconcileUnrealizedCastsPass());
 
         return pm.run (module);
     }
